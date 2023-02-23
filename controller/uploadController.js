492112849685a566927fdcefcdeffed4d634e235
Azizjon <94430020+Azizjon003@users.noreply.github.com>
2023-02-23 11:42:36 +0500

    upload fayllar tayyorlanmoqda

--- controller/uploadController.js
+++ controller/uploadController.js
@@ -1,16 +1,21 @@
 const uploadFile = require("../utility/upload");
 const catchAsync = require("../utility/catchUser");
+const AppError = require("../utility/appError");
 
 const upload = async (req, res, next) => {
-  await uploadFile(req, res);
+  try {
+    await uploadFile(req, res);
 
-  if (req.file == undefined) {
-    return res.status(400).send({ message: "Please upload a file!" });
+    if (req.file == undefined) {
+      return res.status(400).send({ message: "Please upload a file!" });
+    }
+
+    res.status(200).send({
+      message: "Uploaded the file successfully: " + req.file.originalname,
+    });
+  } catch (err) {
+    next(new AppError(err.message, 500));
   }
-
-  res.status(200).send({
-    message: "Uploaded the file successfully: " + req.file.originalname,
-  });
 };
 
 module.exports = {
